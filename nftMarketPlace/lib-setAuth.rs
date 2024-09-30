d93aa504d9036ab2d91e77eaa8580d1cc889dba8
hhzule <hhzule@gmail.com>
2024-09-30 14:26:02 +0500

    updated

--- nftMarketPlace/lib-setAuth.rs
+++ nftMarketPlace/lib-setAuth.rs
@@ -16,18 +16,56 @@ pub mod constants {
     pub const TOKEN_SEED: &[u8] = b"vault";
     pub const NFT_INFO_SEED: &[u8] = b"nft_info";
     pub const PDA_SEED: &[u8] = b"marketplace_pda_seed";
+    pub const FACTORY_CONFIG: &[u8] = b"factory_config";
 }
 
 #[program]
 pub mod nft_market {
     use super::*;
-    pub fn create_item(ctx: Context<CreateItem>, amount: u64, fund_lamports: u64) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, service_fee: u16) -> Result<()> {
+        let factory = &mut ctx.accounts.factory_config;
+        factory.admin = ctx.accounts.admin.key();
+        factory.service_fee = service_fee;
+        factory.fee_collector = ctx.accounts.fee_collector_info.key();
+
+        Ok(())
+    }
+
+    pub fn set_service_fee(ctx: Context<SetServiceFee>, service_fee: u16) -> Result<()> {
+        let factory = &mut ctx.accounts.factory_config;
+        require!(
+            factory.admin == *ctx.accounts.admin.key,
+            ErrorCode::Unauthorized
+        );
+
+        factory.service_fee = service_fee;
+        Ok(())
+    }
+
+    pub fn set_fee_collector(ctx: Context<SetFeeCollector>) -> Result<()> {
+        let factory = &mut ctx.accounts.factory_config;
+        require!(
+            factory.admin == *ctx.accounts.admin.key,
+            ErrorCode::Unauthorized
+        );
+
+        factory.fee_collector = ctx.accounts.fee_collector_info.key();
+        Ok(())
+    }
+
+    pub fn create_item(ctx: Context<CreateItem>, amount: u64) -> Result<()> {
         msg!("Transferring NFT...");
         let nft_info = &mut ctx.accounts.nft_info_account;
         let owner_tk_acc = &ctx.accounts.owner_token_account;
         let user = &ctx.accounts.owner_authority;
         let nft_mint = &ctx.accounts.mint;
+        let factory = &mut ctx.accounts.factory_config;
+        let fee_collector = &mut ctx.accounts.fee_collector;
 
+        require!(
+            factory.fee_collector == *fee_collector.key,
+            ErrorCode::InvalidFeeAccount
+        );
         if nft_mint.supply != 1 {
             return Err(ErrorCode::NotAnNFT.into()); //check if it is single nft
         }
@@ -41,24 +79,41 @@ pub mod nft_market {
             return Err(ErrorCode::MustBeOwnerOfNFT.into()); //check if user owns account
         }
         //crate acc and transfer fund
-        let pda = &mut ctx.accounts.pda_account;
-        let signer = &mut ctx.accounts.owner_authority;
-        let system_program = &ctx.accounts.system_program;
-        let pda_balance_before = pda.get_lamports();
-        Systransfer(
-            CpiContext::new(
-                system_program.to_account_info(),
-                SysTransfer {
-                    from: signer.to_account_info(),
-                    to: pda.to_account_info(),
-                },
-            ),
-            fund_lamports,
+        // let pda = &mut ctx.accounts.pda_account;
+        // let signer = &mut ctx.accounts.owner_authority;
+        // let system_program = &ctx.accounts.system_program;
+        // let pda_balance_before = pda.get_lamports();
+        // Systransfer(
+        //     CpiContext::new(
+        //         system_program.to_account_info(),
+        //         SysTransfer {
+        //             from: signer.to_account_info(),
+        //             to: pda.to_account_info(),
+        //         },
+        //     ),
+        //     fund_lamports,
+        // )?;
+
+        // let pda_balance_after = pda.get_lamports();
+
+        // require_eq!(pda_balance_after, pda_balance_before + fund_lamports);
+
+        // // transfer sol to fee_collector
+        let transfer_instruction = system_instruction::transfer(
+            &user.key(),
+            &fee_collector.key(),
+            factory.service_fee as u64,
+        );
+        // Invoke the transfer instruction
+        anchor_lang::solana_program::program::invoke_signed(
+            &transfer_instruction,
+            &[
+                ctx.accounts.owner_authority.to_account_info(),
+                ctx.accounts.fee_collector.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[],
         )?;
-
-        let pda_balance_after = pda.get_lamports();
-
-        require_eq!(pda_balance_after, pda_balance_before + fund_lamports);
 
         let cpi_context = CpiContext::new(
             ctx.accounts.token_program.to_account_info(),
@@ -83,8 +138,98 @@ pub mod nft_market {
 
         Ok(())
     }
-    pub fn remove_item(ctx: Context<RemoveItem>) -> Result<()> {
+    pub fn update_item(ctx: Context<UpdateItem>, amount: u64) -> Result<()> {
+        msg!("updating NFTlisting...");
+        let nft_info = &mut ctx.accounts.nft_info_account;
+        let owner_tk_acc = &ctx.accounts.owner_token_account;
+        let mint = ctx.accounts.mint.key();
+        let user = ctx.accounts.signer.key();
+        if nft_info.owner.key() != user.key() {
+            return Err(ErrorCode::MustBeOwnerOfNFT.into()); // check the owner of nft
+        }
+        if nft_info.is_listed != true {
+            return Err(ErrorCode::NFTNotListed.into()); // check if nft is listed
+        }
+        if nft_info.sold != false {
+            return Err(ErrorCode::NFTSold.into()); // check if nft is sold
+        }
+        if owner_tk_acc.mint != mint.key() {
+            return Err(ErrorCode::WrongMintAccount.into()); // //check if account belongs to mint
+        }
+
+        nft_info.price = amount;
+        msg!("NFT listing updated successfully.");
+        Ok(())
+    }
+    pub fn purchase_item(ctx: Context<PurchaseItem>, amount: u64) -> Result<()> {
+        let nft_info = &mut ctx.accounts.nft_info_account;
+        let buyer = &ctx.accounts.signer;
+        let factory = &mut ctx.accounts.factory_config;
+        let fee_collector = &mut ctx.accounts.fee_collector;
+        if amount < nft_info.price {
+            return Err(ErrorCode::WrongAmount.into());
+        }
+        if nft_info.is_listed != true {
+            return Err(ErrorCode::NFTNotListed.into()); // check if nft is listed
+        }
+        if nft_info.sold == true {
+            return Err(ErrorCode::NFTSold.into()); // check if nft is sold
+        }
         msg!("Transferring NFT...");
+
+        let bump = &[ctx.bumps.pda_account];
+        let seeds: &[&[u8]] = &[constants::PDA_SEED, bump];
+        let signer_seeds = &[&seeds[..]];
+
+        // // transfer sol to owner
+        let transfer_instruction =
+            system_instruction::transfer(&buyer.key(), &nft_info.owner.key(), nft_info.price);
+        // Invoke the transfer instruction
+        anchor_lang::solana_program::program::invoke_signed(
+            &transfer_instruction,
+            &[
+                ctx.accounts.signer.to_account_info(),
+                ctx.accounts.nft_owner.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[],
+        )?;
+        // // transfer sol to fee_collector
+        let transfer_instruction = system_instruction::transfer(
+            &buyer.key(),
+            &fee_collector.key(),
+            factory.service_fee as u64,
+        );
+        // Invoke the transfer instruction
+        anchor_lang::solana_program::program::invoke_signed(
+            &transfer_instruction,
+            &[
+                ctx.accounts.signer.to_account_info(),
+                ctx.accounts.fee_collector.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[],
+        )?;
+        transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.owner_token_account.to_account_info(),
+                    to: ctx.accounts.buyer_token_account.to_account_info(),
+                    authority: ctx.accounts.pda_account.to_account_info(),
+                },
+                signer_seeds,
+            ),
+            1,
+        )?;
+        nft_info.sold = true;
+        nft_info.owner = buyer.key();
+        nft_info.is_listed = false;
+        msg!("NFT transferred successfully.");
+        Ok(())
+    }
+    pub fn remove_item(ctx: Context<RemoveItem>) -> Result<()> {
+        msg!("removing NFT listing...");
         let nft_info = &mut ctx.accounts.nft_info_account;
         let owner_tk_acc = &ctx.accounts.owner_token_account;
         let mint = ctx.accounts.mint.key();
@@ -119,60 +264,28 @@ pub mod nft_market {
         )?;
 
         nft_info.is_listed = false;
-        msg!("NFT transferred successfully.");
-        Ok(())
-    }
-    pub fn purchase_item(ctx: Context<PurchaseItem>, amount: u64) -> Result<()> {
-        let nft_info = &mut ctx.accounts.nft_info_account;
-        let buyer = &ctx.accounts.signer;
-        if amount < nft_info.price {
-            return Err(ErrorCode::WrongAmount.into());
-        }
-        if nft_info.is_listed != true {
-            return Err(ErrorCode::NFTNotListed.into()); // check if nft is listed
-        }
-        if nft_info.sold == true {
-            return Err(ErrorCode::NFTSold.into()); // check if nft is sold
-        }
-        msg!("Transferring NFT...");
-
-        let bump = &[ctx.bumps.pda_account];
-        let seeds: &[&[u8]] = &[constants::PDA_SEED, bump];
-        let signer_seeds = &[&seeds[..]];
-
-        // // transfer sol to owner
-        let transfer_instruction =
-            system_instruction::transfer(&buyer.key(), &nft_info.owner.key(), nft_info.price);
-        // Invoke the transfer instruction
-        anchor_lang::solana_program::program::invoke_signed(
-            &transfer_instruction,
-            &[
-                ctx.accounts.signer.to_account_info(),
-                ctx.accounts.nft_owner.to_account_info(),
-                ctx.accounts.system_program.to_account_info(),
-            ],
-            &[],
-        )?;
-        transfer(
-            CpiContext::new_with_signer(
-                ctx.accounts.token_program.to_account_info(),
-                Transfer {
-                    from: ctx.accounts.owner_token_account.to_account_info(),
-                    to: ctx.accounts.buyer_token_account.to_account_info(),
-                    authority: ctx.accounts.pda_account.to_account_info(),
-                },
-                signer_seeds,
-            ),
-            1,
-        )?;
-        nft_info.sold = true;
-        nft_info.owner = buyer.key();
-        nft_info.is_listed = false;
-        msg!("NFT transferred successfully.");
+        msg!("NFT listing removed.");
         Ok(())
     }
 }
 
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    #[account(
+        init_if_needed,
+        payer = admin,
+        seeds = [constants::FACTORY_CONFIG],
+        bump,
+        space = 8 + std::mem::size_of::<Factory>()
+    )]
+    pub factory_config: Account<'info, Factory>,
+    /// CHECK
+    #[account(mut)]
+    pub fee_collector_info: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
 #[derive(Accounts)]
 pub struct CreateItem<'info> {
     #[account(mut)]
@@ -198,6 +311,14 @@ pub struct CreateItem<'info> {
     space = 8 + std::mem::size_of::<NftListInfo>()
       )]
     pub nft_info_account: Account<'info, NftListInfo>,
+    #[account(
+        mut,
+        seeds = [constants::FACTORY_CONFIG],
+        bump,
+    )]
+    pub factory_config: Account<'info, Factory>,
+    #[account(mut)]
+    pub fee_collector: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
@@ -250,9 +371,64 @@ pub struct PurchaseItem<'info> {
      bump,
       )]
     pub nft_info_account: Account<'info, NftListInfo>,
+    #[account(
+        mut,
+        seeds = [constants::FACTORY_CONFIG],
+        bump,
+    )]
+    pub factory_config: Account<'info, Factory>,
+    #[account(mut)]
+    pub fee_collector: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
+}
+#[derive(Accounts)]
+pub struct UpdateItem<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub owner_token_account: Account<'info, TokenAccount>,
+    #[account(
+     mut,
+     seeds = [constants::NFT_INFO_SEED, mint.key().as_ref()],
+     bump,
+      )]
+    pub nft_info_account: Account<'info, NftListInfo>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+#[derive(Accounts)]
+pub struct SetServiceFee<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [constants::FACTORY_CONFIG],
+        bump,
+    )]
+    pub factory_config: Account<'info, Factory>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SetFeeCollector<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [constants::FACTORY_CONFIG],
+        bump,
+    )]
+    pub factory_config: Account<'info, Factory>,
+    #[account(mut)]
+    pub fee_collector_info: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
 }
 #[account]
 pub struct NftListInfo {
@@ -264,13 +440,10 @@ pub struct NftListInfo {
     pub mint: Pubkey,
 }
 #[account]
-pub struct CollectionListInfo {
-    pub created_at: u64,
-    pub owner: Pubkey,
-    pub price: u64,
-    pub sold: bool,
-    pub is_listed: bool,
-    pub mint: Pubkey,
+pub struct Factory {
+    pub admin: Pubkey,
+    pub service_fee: u16,
+    pub fee_collector: Pubkey,
 }
 #[error_code]
 pub enum ErrorCode {
@@ -288,4 +461,8 @@ pub enum ErrorCode {
     WrongAmount,
     #[msg("Not Avaiable")]
     NotAvaiable,
+    #[msg("Unauthorized Admin")]
+    Unauthorized,
+    #[msg("Invalid FeeAccount")]
+    InvalidFeeAccount,
 }
--- nftMarketPlace/test/anchor.test.ts
+++ nftMarketPlace/test/anchor.test.ts
@@ -1,101 +1,96 @@
-import { getAssociatedTokenAddressSync } from "@solana/spl-token";
+import {
+  getAssociatedTokenAddressSync,
+  createAssociatedTokenAccount,
+  getOrCreateAssociatedTokenAccount,
+} from "@solana/spl-token";
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+
 describe("NFT Marketplace", async () => {
-  const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
-    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
-  );
   const provider = await anchor.AnchorProvider.env();
   anchor.setProvider(provider);
   const payer = provider.wallet as anchor.Wallet;
   const program = anchor.workspace.NftMarket;
 
+  // it("initialize!", async () => {
+  //   const lamports = anchor.web3.LAMPORTS_PER_SOL;
+  //   let [factory_config] = anchor.web3.PublicKey.findProgramAddressSync(
+  //     [Buffer.from("factory_config")],
+  //     pg.PROGRAM_ID
+  //   );
+  //   console.log(`nftInfo Address: ${factory_config}`);
+  //   const fee_collector_info = new anchor.web3.PublicKey(
+  //     "HpVdgHTUUmCJxc8npDB7YwUPaWpMaccxBBHc13TZqR9B"
+  //   );
+  //   await program.methods
+  //     .initialize(2)
+  //     .accounts({
+  //       admin: pg.wallet.publicKey,
+  //       factoryConfig: factory_config,
+  //       feeCollectorInfo: fee_collector_info,
+  //     })
+  //     .rpc({ skipPreflight: true });
+  // });
   it("list single NFT!", async () => {
-    // Testing constants
+    const lamports = anchor.web3.LAMPORTS_PER_SOL;
     const saleAmount = 1 * anchor.web3.LAMPORTS_PER_SOL;
     const mint: anchor.web3.PublicKey = new anchor.web3.PublicKey(
-      "Hv2fsVxMvUEs1EfskftDJwEVsuKQYpEqXvibPqupkWgV"
+      "CJCWa4fiYzgKm8vtkSTwPuELqPHdJaU9yHCHsC1Lb9EP"
     );
-    const recipient = new anchor.web3.PublicKey(
-      "529jaB5PAPBaoDjtyoD4y55K8B11jWcFY878iQtUtoyt"
-    );
-    // Derive the associated token address account for the mint and payer.
-    // const senderTokenAddress = getAssociatedTokenAddressSync(
-    //   mint,
-    //   payer.publicKey
-    // );
-
     const ownerTokenAddress = await anchor.utils.token.associatedAddress({
       mint: mint,
       owner: payer.publicKey,
     });
-
+    console.log(`Owner's Token Address: ${ownerTokenAddress}`);
     let [nftInfo] = anchor.web3.PublicKey.findProgramAddressSync(
       [Buffer.from("nft_info"), mint.toBuffer()],
       pg.PROGRAM_ID
     );
-    let [RecipientAccount] = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("vault"), mint.toBuffer()],
+    console.log(`nftInfo Address: ${nftInfo}`);
+    let [pdaAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("marketplace_pda_seed")],
       pg.PROGRAM_ID
     );
-    // const metadataAddress = (
-    //   await anchor.web3.PublicKey.findProgramAddressSync(
-    //     [
-    //       Buffer.from("metadata"), 
-    //       TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-    //       mint.toBuffer(),
-    //     ],
-    //     TOKEN_METADATA_PROGRAM_ID
-    //   )
-    // )[0];
-    // console.log(`metadataAddress Address: ${metadataAddress}`);
-    // console.log(`Request to sell NFT: ${mint} for ${saleAmount} lamports.`);
-    console.log(`Owner's Token Address: ${ownerTokenAddress}`);
-    // console.log(`recepient's Token Address: ${RecipientAccount.toString()}`);
-
+    console.log(`pdaAccount Address: ${pdaAccount}`);
+    let [factory_config] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("factory_config")],
+      pg.PROGRAM_ID
+    );
+    console.log(`nftInfo Address: ${factory_config}`);
+    const fee_collector_info = new anchor.web3.PublicKey(
+      "HpVdgHTUUmCJxc8npDB7YwUPaWpMaccxBBHc13TZqR9B"
+    );
     await program.methods
-      .createItem(new BN(100))
+      .createItem(new BN(1))
       .accounts({
         mint: mint,
         ownerTokenAccount: ownerTokenAddress,
         ownerAuthority: pg.wallet.publicKey,
-        // recipientAccount: RecipientAccount,
-        recipientAccount: recipient,
-
+        pdaAccount: pdaAccount,
         nftInfoAccount: nftInfo,
-        // nftMetadata: metadataAddress,
-        // metadataProgram: TOKEN_METADATA_PROGRAM_ID,
+        factoryConfig: factory_config,
+        feeCollector: fee_collector_info,
       })
-      // .signers([pg.wallet])
       .rpc({ skipPreflight: true });
   });
 
   // it("delist!", async () => {
-  //   // Testing constants
   //   const mint: anchor.web3.PublicKey = new anchor.web3.PublicKey(
-  //     "B6ZPVgLCU9v5M3GjEqERn8bjYHvz4MCSeZFtgmsXFLP3"
+  //     "CJCWa4fiYzgKm8vtkSTwPuELqPHdJaU9yHCHsC1Lb9EP"
   //   );
-  //   const recipient = new anchor.web3.PublicKey(
-  //     "529jaB5PAPBaoDjtyoD4y55K8B11jWcFY878iQtUtoyt"
-  //   );
-
   //   const ownerTokenAddress = await anchor.utils.token.associatedAddress({
   //     mint: mint,
   //     owner: pg.wallet.publicKey,
   //   });
   //   let [nftInfo] = anchor.web3.PublicKey.findProgramAddressSync(
-  //     [
-  //       Buffer.from("nft_info"),
-  //       mint.toBuffer(),
-  //     ],
-  //     pg.PROGRAM_ID
-  //   );
-  //   let [RecipientAccount] = anchor.web3.PublicKey.findProgramAddressSync(
-  //     [Buffer.from("vault"), mint.toBuffer()],
+  //     [Buffer.from("nft_info"), mint.toBuffer()],
   //     pg.PROGRAM_ID
   //   );
   //   console.log(`Owner's Token Address: ${ownerTokenAddress}`);
-  //   console.log(`RecipientAccount's Token Address: ${RecipientAccount}`);
+  //   let [pdaAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+  //     [Buffer.from("marketplace_pda_seed")],
+  //     pg.PROGRAM_ID
+  //   );
   //   await program.methods
   //     .removeItem()
   //     .accounts({
@@ -103,149 +98,72 @@ describe("NFT Marketplace", async () => {
   //       signer: pg.wallet.publicKey,
   //       nftInfoAccount: nftInfo,
   //       ownerTokenAccount: ownerTokenAddress,
-  //       recipientAccount: RecipientAccount,
+  //       pdaAccount: pdaAccount,
   //     })
-  //     // .signers([pg.wallet])
   //     .rpc({ skipPreflight: true });
   // });
 
-  // it("purchase single NFT!", async () => {
-  //   // Testing constants
-  //   const saleAmount = 1 * anchor.web3.LAMPORTS_PER_SOL;
-  //   // const mint: anchor.web3.PublicKey = new anchor.web3.PublicKey(
-  //   //   "B6ZPVgLCU9v5M3GjEqERn8bjYHvz4MCSeZFtgmsXFLP3"
-  //   // );
-  //   //     const mint: anchor.web3.PublicKey = new anchor.web3.PublicKey(
-  //   //   "Hv2fsVxMvUEs1EfskftDJwEVsuKQYpEqXvibPqupkWgV"
-  //   // );
-  //       const mint: anchor.web3.PublicKey = new anchor.web3.PublicKey(
-  //     "5pTZWfoprGLHHdvrEFT6AheNtf5PrnBTu6XHeR3YcBjU"
-  //   );
-  //   const buyer = new anchor.web3.PublicKey(
-  //     "5MjuAE8spr7DidyaYg6uNhaJ7Jo5FYmwWodjRKyZDfuk"
-  //   );
-  //   const buyerTokenAddress = await anchor.utils.token.associatedAddress({
-  //     mint: mint,
-  //     owner: buyer,
-  //   });
+  it("purchase single NFT!", async () => {
+    const mint: anchor.web3.PublicKey = new anchor.web3.PublicKey(
+      "2EN7hgfk3nXehjwYKekhsQvcyL73BuEiBYQ2b6x6yJK8"
+    );
+    const buyer = new anchor.web3.PublicKey(
+      "HpVdgHTUUmCJxc8npDB7YwUPaWpMaccxBBHc13TZqR9B"
+    );
+    let tokenAcc = await getOrCreateAssociatedTokenAccount(
+      pg.connection,
+      payer.payer,
+      mint,
+      buyer
+    );
+    console.log(`tokenAcc Address: ${tokenAcc.address}`);
+    const buyerTokenAddress = await anchor.utils.token.associatedAddress({
+      mint: mint,
+      owner: buyer,
+    });
+    console.log(`Buyer's Token Address: ${buyerTokenAddress}`);
+    const nft_owner = new anchor.web3.PublicKey(
+      "5MjuAE8spr7DidyaYg6uNhaJ7Jo5FYmwWodjRKyZDfuk"
+    );
+    const ownerTokenAddress = await anchor.utils.token.associatedAddress({
+      mint: mint,
+      owner: nft_owner,
+    });
+    console.log(`ownerTokenAddress Address: ${ownerTokenAddress}`);
 
-  //   const nft_owner = new anchor.web3.PublicKey(
-  //     "5MjuAE8spr7DidyaYg6uNhaJ7Jo5FYmwWodjRKyZDfuk"
-  //   );
-  //   // const ownerTokenAddress = await anchor.utils.token.associatedAddress({
-  //   //   mint: mint,
-  //   //   owner: payer.publicKey,
-  //   // });
+    let [nftInfo] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("nft_info"), mint.toBuffer()],
+      pg.PROGRAM_ID
+    );
+    let [pdaAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("marketplace_pda_seed")],
+      pg.PROGRAM_ID
+    );
+    console.log(`pdaAccount Address: ${pdaAccount}`);
+    let [factory_config] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("factory_config")],
+      pg.PROGRAM_ID
+    );
+    console.log(`nftInfo Address: ${factory_config}`);
+    const fee_collector_info = new anchor.web3.PublicKey(
+      "HpVdgHTUUmCJxc8npDB7YwUPaWpMaccxBBHc13TZqR9B"
+    );
+    await program.methods
+      .purchaseItem(new BN(2))
+      .accounts({
+        signer: pg.wallet.publicKey,
+        mint: mint,
+        nftOwner: nft_owner,
+        buyerTokenAccount: buyerTokenAddress,
+        pdaAccount: pdaAccount,
+        nftInfoAccount: nftInfo,
+        ownerTokenAccount: ownerTokenAddress,
+        factoryConfig: factory_config,
+        feeCollector: fee_collector_info,
+      })
 
-  //   let [nftInfo] = anchor.web3.PublicKey.findProgramAddressSync(
-  //     [Buffer.from("nft_info"), mint.toBuffer()],
-  //     pg.PROGRAM_ID
-  //   );
-
-  //   let [programAccount] = anchor.web3.PublicKey.findProgramAddressSync(
-  //     [Buffer.from("vault"), mint.toBuffer()],
-  //     pg.PROGRAM_ID
-  //   );
-  //   // const metadataAddress = (
-  //   //   await anchor.web3.PublicKey.findProgramAddressSync(
-  //   //     [
-  //   //       Buffer.from("metadata"),
-  //   //       TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-  //   //       mint.toBuffer(),
-  //   //     ],
-  //   //     TOKEN_METADATA_PROGRAM_ID
-  //   //   )
-  //   // )[0];
-  //   // console.log(`metadataAddress Address: ${metadataAddress}`);
-  //   // console.log(`Request to sell NFT: ${mint} for ${saleAmount} lamports.`);
-  //   console.log(`Buyer's Token Address: ${buyerTokenAddress}`);
-
-  //   await program.methods
-  //     .purchaseItem(new BN(100))
-  //     .accounts({
-  //       signer: pg.wallet.publicKey,
-  //       mint: mint,
-  //       nftOwner: nft_owner,
-  //       buyerTokenAccount: buyerTokenAddress,
-  //       programNftAccount: programAccount,
-  //       nftInfoAccount: nftInfo,
-  //     })
-  //     // .signers([pg.wallet])
-  //     .rpc({ skipPreflight: true });
-  // });
+      // .signers([payer.payer])
+      .rpc({ skipPreflight: true });
+  });
 });
 ///////////////////////////
-
-////////////////////////////////////////////////////////
-// it("Create an NFT!", async (done) => {
-//   // Generate a keypair to use as the address of our mint account
-//   const mintKeypair = anchor.web3.Keypair.generate();
-//   console.log("mintKeypair initialized", mintKeypair.publicKey.toString());
-//   const metadataAddress = (
-//     await anchor.web3.PublicKey.findProgramAddressSync(
-//       [
-//         Buffer.from("metadata"),
-//         TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-//         mintKeypair.publicKey.toBuffer(),
-//       ],
-//       TOKEN_METADATA_PROGRAM_ID
-//     )
-//   )[0];
-//   console.log("Metadata initialized", metadataAddress.toString());
-//   const masterEditionAddress = (
-//     await anchor.web3.PublicKey.findProgramAddressSync(
-//       [
-//         Buffer.from("metadata"),
-//         TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-//         mintKeypair.publicKey.toBuffer(),
-//         Buffer.from("edition"),
-//       ],
-//       TOKEN_METADATA_PROGRAM_ID
-//     )
-//   )[0];
-//   console.log(
-//     "Master edition metadata initialized",
-//     masterEditionAddress.toString()
-//   );
-
-//   // // Derive the associated token address account for the mint_authority/payer/owner.
-//   const associatedTokenAccountAddress = getAssociatedTokenAddressSync(
-//     mintKeypair.publicKey,
-//     payer.publicKey
-//   );
-//   console.log(
-//     "associatedTokenAccountAddress",
-//     associatedTokenAccountAddress.toString()
-//   );
-//   // // Derive the associated token address account for the program.
-//   const associatedTokenAccountAddressProgram = getAssociatedTokenAddressSync(
-//     mintKeypair.publicKey,
-//     payer.publicKey
-//   );
-//   console.log(
-//     "associatedTokenAccountAddress",
-//     associatedTokenAccountAddress.toString()
-//   );
-//   const transactionSignature = await program.methods
-//     .createNftItem(metadata.name, metadata.symbol, metadata.uri)
-//     .accounts({
-//       // signer: pg.wallet,
-//       signer: mintKeypair,
-//       mint: mintKeypair.publicKey,
-//       associatedTokenAccount: associatedTokenAccountAddress,
-//       masterEditionAccount: masterEditionAddress,
-//       metadataAccount: metadataAddress,
-//       tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
-
-//       //       tokenProgram: pg.TOKEN_PROGRAM_ID,
-//       // associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
-//       systemProgram: web3.SystemProgram.programId,
-//       rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-//     })
-//     .signers([mintKeypair])
-//     .rpc({ skipPreflight: true });
-//   done();
-//   console.log("Success!");
-//   console.log(`   Mint Address: ${mintKeypair.publicKey}`);
-//   console.log(`   Transaction Signature: ${transactionSignature}`);
-// }).timeout(10000);
